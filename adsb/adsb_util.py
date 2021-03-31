9ee88ea9b3f2e427fea8e3e8d5a1a06eae9b4eb1
Mitesh Kumar <miteshkumarca@gmail.com>
2021-03-30 21:23:49 -0400

    Add flight distance relationship

--- adsb/adsb_util.py
+++ adsb/adsb_util.py
@@ -5,6 +5,8 @@ import pandas as pd
 from dataclasses import make_dataclass
 
 
+
+
 class flightVector:
     def __init__(self, id):
         self.id = id
@@ -14,6 +16,9 @@ class flightVector:
         self.stationtime = list()
         self.latency = list()
         self.altitudes = list()
+        self.mlat_server = True
+        self.distances = list()
+        self.military = True
 
 
 def pfs(fpath):
@@ -45,20 +50,34 @@ def pfs(fpath):
             else:
                 ftable[fobj['Id']].altitudes.append(0)
 
+            if 'Mlat' in fobj:
+              ftable[fobj['Id']].mlat_server = fobj['Mlat']
+            if 'Mil' in fobj:
+              ftable[fobj['Id']].military = fobj['Mil']
+            
+            if 'Dst' in fobj:
+              ftable[fobj['Id']].distances.append(fobj['Dst'])
+            elif len(ftable[fobj['Id']].distances) == 0:
+              ftable[fobj['Id']].distances.append(0)
+            else:
+              ftable[fobj['Id']].distances.append(ftable[fobj['Id']].distances[-1])
+
     return ftable
 
 
 def tdf(flightVectors):
     FlightPoint = make_dataclass("FlightPoint", [("Id", int), ("Lat", float), ("Long", float), (
-        "Flighttime", int), ("Stationtime", int), ("Latency", int), ("Altitude", int)])
-    # return pd.DataFrame([FlightPoint()])
+        "Flighttime", int), ("Stationtime", int), ("Latency", int), ("Altitude", int), ("Military", bool), ("Distance", int), ("Mlat", bool)])
+
     data = []
     for row in flightVectors.values():
-        data.extend([FlightPoint(row.id, lat, long_, flighttime, stationtime, latency, altitude) for lat, long_, flighttime,
-                    stationtime, latency, altitude in zip(row.lats, row.longs, row.flighttime, row.stationtime, row.latency, row.altitudes)])
+        data.extend([FlightPoint(row.id, lat, long_, flighttime, stationtime, latency, altitude, row.military, distance, row.mlat_server) for lat, long_, flighttime,
+                    stationtime, latency, altitude, distance in zip(row.lats, row.longs, row.flighttime, row.stationtime, row.latency, row.altitudes, row.distances)])
     return pd.DataFrame(data)
 
 
 def clean(flightDf):
     flightDf = flightDf[flightDf.Latency > 0] # latency less than 0 doesn't make sense
+    flightDf = flightDf[flightDf.Altitude > 0]
+    flightDf = flightDf[flightDf.Altitude < 45000]
     return flightDf
